449adeb0202abe7f641b474ec4f51f3fb5827953
peter88213 <peter.88213@t-online.de>
2023-06-09 12:56:17 +0200

    Update docstring

--- tools/build_yw2xtg.py
+++ tools/build_yw2xtg.py
@@ -1,4 +1,4 @@
-""" Build a python script for the yw2xtg distribution.
+"""Build a Python script for the yw2xtg distribution.
         
 In order to distribute a single script without dependencies, 
 this script "inlines" all modules imported from the pywriter package.
@@ -6,6 +6,7 @@ this script "inlines" all modules imported from the pywriter package.
 The PyWriter project (see see https://github.com/peter88213/PyWriter)
 must be located on the same directory level as the yw2xtg project. 
 
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/yw2xtg
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
